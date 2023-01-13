1108723945a1b908be935e1b4063514fdc1c3dd0
abubakkariiui <abubakkarmit@gmail.com>
2023-01-13 10:27:22 +0500

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import { logInRoute } from './logInRoute.js';
+import { signUpRoute } from './signUpRoute.js';
+import { testRoute } from './testRoute.js';
+import { updateUserInfoRoute } from './updateUserInfoRoute.js';
+import { verifyEmailRoute } from './verifyEmailRoute.js';
+
+export const routes = [
+    logInRoute,
+    signUpRoute,
+    testRoute,
+    updateUserInfoRoute,
+    verifyEmailRoute,
+];
